0895da34e28715c60b7fbe739f5c65c71bd26f24
Lewis Luo <ts03002215@gmail.com>
2020-10-25 18:50:16 +0800

    code: fix npm can't run seed script

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -4,8 +4,16 @@ const categorySeed = require('./data/category')
 
 db.once('open', () => {
   for (let i = 0; i < categorySeed.length; i++) {
-    Category.create(categorySeed[i])
-    console.log(`分類種子: 第${i + 1}筆資料完成`)
+    if (i < categorySeed.length - 1) {
+      Category.create(categorySeed[i])
+      console.log(`分類種子: 第${i + 1}筆資料完成`)
+    } else if (i === categorySeed.length - 1) {
+      Category.create(categorySeed[i])
+        .then(() => {
+          console.log(`分類種子: 第${i + 1}筆資料完成`)
+          console.log('done.')
+          db.close()
+        })
+    }
   }
-  console.log('done.')
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -4,8 +4,16 @@ const recordSeed = require('./data/record')
 
 db.once('open', () => {
   for (let i = 0; i < recordSeed.length; i++) {
-    Record.create(recordSeed[i])
-    console.log(`紀錄種子: 第${i + 1}筆資料完成`)
+    if (i < recordSeed.length - 1) {
+      Record.create(recordSeed[i])
+      console.log(`紀錄種子: 第${i + 1}筆資料完成`)
+    } else if (i === recordSeed.length - 1) {
+      Record.create(recordSeed[i])
+        .then(() => {
+          console.log(`紀錄種子: 第${i + 1}筆資料完成`)
+          console.log('done.')
+          db.close()
+        })
+    }
   }
-  console.log('done.')
 })
